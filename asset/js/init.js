87c3bb30815e2c9dbb087cd1d321764abd7282fc
Khalushankar <113584882+Khalushankar@users.noreply.github.com>
2023-02-13 21:18:43 +0530

    Update init.js

--- asset/js/init.js
+++ asset/js/init.js
@@ -7,8 +7,8 @@
       controlNav: true,
       directionNav: false,
       smoothHeight: true,
-      slideshowSpeed: 2000,
-      animationSpeed: 200,
+      slideshowSpeed: 5000,
+      animationSpeed: 100,
       randomize: false
    });
 });
